53dc589d1fa855ce8c421fb178f7371531130e81
Alejandro Alonso López <aalonsopersonal@gmail.com>
2024-09-18 18:23:53 +0200

    Added lens name on highlight

--- pregnancy-lens.js
+++ pregnancy-lens.js
@@ -14,6 +14,7 @@ let annotationProcess = (listOfCategories, enhanceTag, document, response) => {
             let elements = document.getElementsByClassName(check);
             for (let i = 0; i < elements.length; i++) {
                 elements[i].classList.add(enhanceTag);
+                elements[i].classList.add("pregnancy-lens");
             }
             if (document.getElementsByTagName("head").length > 0) {
                 document.getElementsByTagName("head")[0].remove();
